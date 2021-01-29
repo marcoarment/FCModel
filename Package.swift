2ab37f31307640fb8d3e9d28180e037a30eaacc8
Cihat Gündüz <commits@cihatguenduez.de>
2021-01-29 15:48:35 +0100

    Add basic SwiftPM support for Objective-C code

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "FCModel",
+    platforms: [.macOS(.v10_10), .iOS(.v9)],
+    products: [.library(name: "FCModel", targets: ["FCModel"])],
+    dependencies: [
+      .package(name: "FMDB", url: "https://github.com/ccgus/fmdb.git", from: "2.7.7"),
+    ],
+    targets: [
+        .target(
+            name: "FCModel",
+            dependencies: ["FMDB"],
+            path: "FCModel",
+            exclude: ["FCModel+ObservableObject.swift"],
+            publicHeadersPath: "include"
+        )
+    ]
+)
